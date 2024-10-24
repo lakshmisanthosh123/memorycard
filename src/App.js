5fcd7a57a61b4b56f3b647078468d4c398646a41
lakshmisanthosh123 <lakshmisanthosh1994@gmail.com>
2024-10-24 11:45:01 +0530

    memory card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, {useState,useEffect}from 'react';
+
 import './App.css';
 
 function App() {
+  const cardData = [
+    { id: 1, value: '🐶' },
+    { id: 2, value: '🐱' },
+    { id: 3, value: '🐭' },
+    { id: 4, value: '🐹' },
+    { id: 5, value: '🐰' },
+    { id: 6, value: '🦊' },
+    { id: 7, value: '🐻' },
+    { id: 8, value: '🐼' }
+  ];
+  const [cards, setCards] = useState([]);
+  const [moves, setMoves] = useState(0);
+  const [matches, setMatches] = useState(0);
+
+  
+  const handlecardclick=(id)=>{
+const isFlipped = cardData.find(card=>card.id===id);
+if(isFlipped || moves.length===2);
+setMoves([...moves,id])
+if(isFlipped.length===1){
+  if(moves.length===1){
+    const firstindex=moves[0];
+    if(cardData[firstindex]===cardData[id]){
+      setMatches([...matches,firstindex,id])
+    }
+  }
+}
+  }
+  
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <h1>Memory Card Game</h1>
+      <div>
+        
+       {cardData.map((card)=>{
+<div
+key={card.id}
+
+onClick={()=>handlecardclick(card.id)}
+>{card.value}</div>
+       })}
+      </div>
+     
+       
     </div>
   );
 }
